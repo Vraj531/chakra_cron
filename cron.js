1ca7d5b1cdc2ab16c292c6169c8a5545a01522e0
Vraj Patel <53927749+Vraj531@users.noreply.github.com>
2024-08-03 18:32:17 -0400

    Update cron.js

--- cron.js
+++ cron.js
@@ -1,17 +1,21 @@
 const cron = require('node-cron');
 const { exec } = require('child_process');
 
-cron.schedule('*/3 * * * *', () => {
-    console.log('Pinging Lambda API...');
-    exec('node pingLambda.js', (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Error executing script: ${error}`);
-            return;
-        }
-        if (stderr) {
-            console.error(`Script error: ${stderr}`);
-            return;
-        }
-        console.log(`Script output: ${stdout}`);
+module.exports = (req, res) => {
+    cron.schedule('*/3 * * * *', () => {
+        console.log('Pinging Lambda API...');
+        exec('node pingLambda.js', (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Error executing script: ${error}`);
+                return;
+            }
+            if (stderr) {
+                console.error(`Script error: ${stderr}`);
+                return;
+            }
+            console.log(`Script output: ${stdout}`);
+        });
     });
-});
+    
+    res.status(200).send('Cron job is set up.');
+};
